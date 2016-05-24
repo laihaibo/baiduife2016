b55549156b4c43fcd386869eaa4b8eec50f5b321
laihaibo <18768101327@163.com>
2016-05-24 15:33:14 +0800

    task34 update

--- part2/task34/task34.js
+++ part2/task34/task34.js
@@ -1,9 +1,9 @@
 /*
-* @Author: laihaibo
-* @Date:   2016-05-24 14:12:56
-* @Last Modified by:   laihaibo
-* @Last Modified time: 2016-05-24 14:13:08
-*/
+ * @Author: laihaibo
+ * @Date:   2016-05-24 14:12:56
+ * @Last Modified by:   laihaibo
+ * @Last Modified time: 2016-05-24 15:32:34
+ */
 
 window.onload = function() {
 
@@ -20,10 +20,14 @@ window.onload = function() {
   var current = 0;
   var directionCount = [0];
   var square = document.getElementById('square');
-  var go = document.getElementById('go');
-  var tunlef = document.getElementById('tunlef');
-  var tunrig = document.getElementById('tunrig');
-  var tunbac = document.getElementById('tunbac');
+  // var traTop = document.getElementById('traTop');
+  // var traLef = document.getElementById('traLef');
+  // var traBot = document.getElementById('traBot');
+  // var traRig = document.getElementById('traRig');
+  // var movTop = document.getElementById('movTop');
+  // var movLef = document.getElementById('movLef');
+  // var movBot = document.getElementById('movBot');
+  // var movRig = document.getElementById('movRig');
 
   function goCommand() {
     // current=(current+90)%360;
@@ -48,18 +52,8 @@ window.onload = function() {
     }
 
     //设置边缘的范围
-    if (countLeft <= 0) {
-      countLeft = 0;
-    }
-    if (countTop <= 0) {
-      countTop = 0;
-    }
-    if (countTop >= 400) {
-      countTop = 400;
-    }
-    if (countLeft >= 400) {
-      countLeft = 400;
-    }
+    countTop = checkRange(countTop);
+    countLeft = checkRange(countLeft);
     // this.style.transform='rotate('+current+'deg)';
     // directionCount[0]=current;
     // console.log(directionCount[0]);
@@ -68,44 +62,100 @@ window.onload = function() {
     square.style.left = countLeft + 'px';
   };
 
-  function tunbacCommand() {
-    current = (current + 180) % 360;
-    square.style.transform = 'rotate(' + current + 'deg)';
-    directionCount[0] = current;
-  };
-
-  function tunrigCommand() {
-    current = (current + 90) % 360;
-    square.style.transform = 'rotate(' + current + 'deg)';
-    directionCount[0] = current;
-  };
-
-  function tunlefCommand() {
-    current = (current + 270) % 360;
-    square.style.transform = 'rotate(' + current + 'deg)';
-    directionCount[0] = current;
-  };
-
-  addEvent(go, 'click', goCommand);
-  addEvent(tunlef, 'click', tunlefCommand);
-  addEvent(tunrig, 'click', tunrigCommand);
-  addEvent(tunbac, 'click', tunbacCommand);
-  //增加键盘
-  addEvent(document, 'keydown', function(e) {
-    var event = e || window.event;
-    switch (event.keyCode) {
-      case 37:
-        tunlefCommand();
-        break;
-      case 38:
-        goCommand();
-        break;
-      case 39:
-        tunrigCommand();
-        break;
-      case 40:
-        tunbacCommand();
-        break;
+  function checkRange(num) {
+    if (num <= 0) {
+      num = 0;
     }
-  });
+    if (num >= 400) {
+      num = 400;
+    }
+    return num;
+  }
+
+
+  var squObj = {
+    gotraTop: function() {
+      directionCount[0] = 0;
+      goCommand();
+    },
+
+    gotraBot: function() {
+      directionCount[0] = 180;
+      goCommand();
+    },
+    gotraLef: function() {
+      directionCount[0] = 270;
+      goCommand();
+    },
+    gotraRig: function() {
+      directionCount[0] = 90;
+      goCommand();
+    },
+
+    gomovTop: function() {
+      current = 0;
+      square.style.transform = 'rotate(' + current + 'deg)';
+      directionCount[0] = current;
+      squObj.gotraTop();
+    },
+    gomovBot: function() {
+      current = 180;
+      square.style.transform = 'rotate(' + current + 'deg)';
+      directionCount[0] = current;
+      squObj.gotraBot();
+    },
+
+    gomovRig: function() {
+      current = 90;
+      square.style.transform = 'rotate(' + current + 'deg)';
+      directionCount[0] = current;
+      squObj.gotraRig();
+    },
+
+    gomovLef: function() {
+      current = 270;
+      square.style.transform = 'rotate(' + current + 'deg)';
+      directionCount[0] = current;
+      squObj.gotraLef();
+    },
+  }
+
+  //addEvent(traTop, 'click', pp['gotraTop']);
+  // addEvent(traLef, 'click', gotraLef);
+  // addEvent(traRig, 'click', gotraRig);
+  // addEvent(traBot, 'click', gotraBot);
+  // addEvent(movTop, 'click', gomovTop);
+  // addEvent(movLef, 'click', gomovLef);
+  // addEvent(movRig, 'click', gomovRig);
+  // addEvent(movBot, 'click', gomovBot);
+  //增加键盘
+  // addEvent(document, 'keydown', function(e) {
+  //   var event = e || window.event;
+  //   switch (event.keyCode) {
+  //     case 37:
+  //       tunlefCommand();
+  //       break;
+  //     case 38:
+  //       goCommand();
+  //       break;
+  //     case 39:
+  //       tunrigCommand();
+  //       break;
+  //     case 40:
+  //       tunbacCommand();
+  //       break;
+  //   }
+  // });
+
+
+
+  function addBtnHandle(e) {
+    if (e.target.nodeName == 'BUTTON') {
+      var btn = e.target;
+      var action = 'go' + e.target.value;
+      addEvent(btn, 'click', squObj[action]);
+    }
+  }
+  var control = document.getElementById('control');
+  addEvent(control, 'mouseover', addBtnHandle);
 }
